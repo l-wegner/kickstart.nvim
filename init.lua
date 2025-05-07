b790112d1fa4bf6e23b293845f19c33ac95f4676
Lars Wegner <lars.wegner.devs@gmail.com>
2025-05-07 12:05:42 +0200

    Add config/plugins folder loading by lazy
    Signed-off-by: Lars Wegner <lars.wegner.devs@gmail.com>

--- init.lua
+++ init.lua
@@ -239,6 +239,7 @@ require 'config.lazy'
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  { import = 'config.plugins' },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
