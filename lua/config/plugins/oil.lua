f5914d50bd48adf634f3a645b22d6545d34fb77b
Lars Wegner <lars.wegner.devs@gmail.com>
2025-05-22 09:56:03 +0200

    Configure oil to show hidden files and permissions
    Signed-off-by: Lars Wegner <lars.wegner.devs@gmail.com>

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -3,7 +3,15 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      columns = {
+        'icons',
+        'permissions',
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    },
     -- Optional dependencies
     -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
