c6da7fa7d2f23c520f109c375fd1d0f099938ae4
Lars Wegner <lars.wegner.devs@gmail.com>
2025-07-08 09:09:49 +0200

    Update rest-nvim config to ignore ssl verification
    Signed-off-by: Lars Wegner <lars.wegner.devs@gmail.com>

--- lua/config/plugins/rest.lua
+++ lua/config/plugins/rest.lua
@@ -8,9 +8,16 @@ return {
         table.insert(opts.ensure_installed, 'http')
       end,
     },
+    --   opts = {
+    --   request = {
+    --   skip_ssl_verification = true,
+    --      },
+    --},
     config = function()
       vim.api.nvim_set_keymap('n', '<leader>rr', '<cmd>Rest run<CR>', { noremap = true, silent = true })
-      vim.api.nvim_set_keymap('n', '<leader>rl', '<cmd>Rest last<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>ra', '<cmd>Rest last<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>rl', '<cmd>Rest logs<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>ro', '<cmd>Rest open<CR>', { noremap = true, silent = true })
       vim.api.nvim_set_keymap('n', '<leader>re', '<cmd>Telescope rest select_env<CR>', { noremap = true, silent = true })
     end,
   }, -- Example keybindings for rest.nvim
